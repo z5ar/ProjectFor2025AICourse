93681313178c7739eb1db9aa660486a4d2bfbce5
Zhong Mingjie <412052582@qq.com>
2025-07-08 15:18:24 +0800

    Generator: change to Whisper

--- src/subtitle_generator/_generate.py
+++ src/subtitle_generator/_generate.py
@@ -1,12 +1,11 @@
 from time import time
 start_time=time()
-import torch, torchaudio
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+import whisper, os
 from pyannote.audio import Pipeline
 from moviepy.video.io.VideoFileClip import VideoFileClip
-import os, re
 from datetime import timedelta
 print(f'Imported in {time()-start_time}s.')
+from pprint import pprint
 del start_time
 
 def mytimer(func):
@@ -18,23 +17,22 @@ def mytimer(func):
     return ret
 
 class SubtitleGenerator(object):
-    def __init__(self,token,device='cuda' if torch.cuda.is_available() else 'cpu'):
-        self.device=torch.device(device)
+    def __init__(self,token):
         self.token=token
         self.asr_processor=None
         self.asr_model=None
         self.diarization_pipeline=None
-        print(f'{self.device=}\n{self.token=}')
+        print(f'{self.token=}')
     
     @mytimer
     def load_models(self):
         print('Loading speech recognition model...')
-        self.asr_processor=Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
-        self.asr_model=Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h-lv60-self").to(self.device)
+        self.whisper_model=whisper.load_model('large-v3-turbo',download_root='./models/whisper')
         print(f'Loading speaker diarization model...')
         self.diarization_pipeline = Pipeline.from_pretrained(
             "pyannote/speaker-diarization-3.1",
-            use_auth_token=self.token
+            use_auth_token=self.token,
+            cache_dir='./models/pyannote'
         )
 
     @mytimer
@@ -45,87 +43,85 @@ class SubtitleGenerator(object):
             print('STEP 1: Extract audio...')
             video=VideoFileClip(src_video)
             video.audio.write_audiofile(mid_audio,fps=16000,codec='pcm_s16le')
-            audio_duration=video.audio.duration
             video.close()
-            return audio_duration
 
         @mytimer
         def diarize_speakers(mid_audio:str):
             # 区分说话人
             print("STEP 2: Diarize speakers...")
             diarization=self.diarization_pipeline(mid_audio)
-            speaker_segments=list()
+            speaker_segments=dict()
+            # 对时间段以speaker分类便于合并
             for segment, _, speaker in diarization.itertracks(yield_label=True):
-                speaker_segments.append({
-                    "start": segment.start,
-                    "end": segment.end,
-                    "speaker": speaker
-                })
-            return speaker_segments
+                speaker_segments[speaker]=speaker_segments.get(speaker,())+((segment.start,segment.end),)
+            for speaker in speaker_segments.keys():
+                segments=speaker_segments[speaker]
+                segments=sorted(segments, key=lambda x:x[0])
+                new_segments=list()
+                last_start=segments[0][0]
+                last_end=segments[0][1]
+                for elem in segments[1:]:
+                    if elem[0]-last_end>1:
+                        new_segments.append((last_start,last_end))
+                        last_start=elem[0]
+                    last_end=elem[1]
+                    
+                new_segments.append((last_start,last_end))
+                speaker_segments[speaker]=new_segments
+            ret=list()
+            # 转为以时间作键便于后续查找对应
+            for speaker, periods in speaker_segments.items():
+                for period in periods:
+                    ret.append({
+                        'start':period[0],
+                        'end':period[1],
+                        'speaker':speaker
+                    })
+            ret=sorted(ret,key=lambda x:x['start'])
+            pprint(ret)
+            return ret
         
         @mytimer
         def transcribe_audio(mid_audio:str):
             # 语音识别
             print('STEP 3: Transcribe audio...')
-            waveform, sample_rate=torchaudio.load(mid_audio)
-            if sample_rate!=16000:
-                resampler=torchaudio.transforms.Resample(sample_rate)
-                waveform=resampler(waveform)
-            
-            input_values=self.asr_processor(
-                waveform.squeeze().numpy(),
-                sampling_rate=16000,
-                return_tensors='pt'
-            ).input_values.to(self.device)
-
-            with torch.no_grad():
-                logits=self.asr_model(input_values).logits
-            
-            predicted_ids=torch.argmax(logits,dim=-1)
-            transcription=self.asr_processor.batch_decode(predicted_ids)[0].lower()
-            print(transcription)
-            return transcription
+            result=self.whisper_model.transcribe(mid_audio)
+            ret=list()
+            for segment in result['segments']:
+                ret.append({
+                    'start': segment['start'],
+                    'end': segment['end'],
+                    'text': segment['text']
+                })
+            ret=sorted(ret,key=lambda x:x['start'])
+            pprint(ret)
+            return ret
 
         @mytimer
-        def align(audio_duration, speaker_segments, transcription):
+        def align(speaker_segments:list, transcription_result:list):
             # 对齐文本与说话人
             print('STEP 4: Align transcription with speaker segments...')
-            words=re.findall(r'\b\w+\b',transcription)
-            word_duration=audio_duration/len(words)
-
-            word_timestamps=list()
-            for i,word in enumerate(words):
-                begin_time=i*word_duration
-                end_time=(i+1)*word_duration
-                word_timestamps.append({
-                    "word":word,
-                    "start":begin_time,
-                    "end":end_time
-                })
-
-            aligned_segments=list()
-            for segment in speaker_segments:
-                segment_text=list()
-                for word in word_timestamps:
-                    if word["start"]>=segment['start'] and word['end']<segment['end']:
-                        segment_text.append(word['word'])
-                
-                if segment_text:
-                    aligned_segments.append({
-                        "speaker": segment["speaker"],
-                        "start": segment['start'],
-                        "end": segment["end"],
-                        "text": " ".join(segment_text)
-                    })
-
+            def contains(a,b):
+                return (a['start']-1 <= b['start']) and (a['end']+1 >= b['end'])
+            for i,line in enumerate(transcription_result):
+                for j,segment in enumerate(speaker_segments):
+                    if contains(segment,line):
+                        line['speaker']=segment['speaker']
+                        transcription_result[i]=line
+                        break
+                else:
+                    line['speaker']='BAD_ALIGNMENT'
+            print(transcription_result)
+            return transcription_result
+                        
         @mytimer
         def output_subtitle(dist_subtitle, aligned_segments):
             # 生成字幕
             print('STEP 5: Output into SRT files...')
             with open(dist_subtitle,"w",encoding='utf-8') as f:
                 def format_timedelta(td: timedelta):
-                    hours=td.seconds//3600
-                    minutes=td.seconds%60//60
+                    hours=td.seconds//60//60
+                    minutes=td.seconds//60%60
                     seconds=td.seconds%60%60
                     ms=td.microseconds//1000
                     return f'{hours:02}:{minutes:02}:{seconds:02}, {ms:03}'
@@ -142,23 +138,30 @@ class SubtitleGenerator(object):
             print('STEP 6: Clean temporary files...')
             os.remove(mid_audio)
 
+
         mid_audio='temp_audio.wav'
-        audio_duration = extract_audio(src_video,mid_audio)
-        
+        extract_audio(src_video,mid_audio)
         output_subtitle(
             dist_subtitle=dist_subtitle,
             aligned_segments=align(
-                audio_duration=audio_duration,
                 speaker_segments=diarize_speakers(mid_audio),
-                transcription=transcribe_audio(mid_audio)
+                transcription_result=transcribe_audio(mid_audio)
             )
         )
-
+        clean(mid_audio)
         print(f"Done...\n\tSrc: {src_video}\n\tDist:{dist_subtitle}")
 
 if __name__=='__main__':
-    with open('TOKEN','r') as f:
-        token=f.readline()
+    while True:
+        try:
+            with open('TOKEN','r') as f:
+                token=f.readline()
+        except FileNotFoundError:
+            path=input('>>> ')
+            os.chdir(path)
+        else:
+            break
+    
     generator=SubtitleGenerator(token)
     generator.load_models()
     generator.generate('test.mp4','test.srt')
